3a951989c0d39097034b94aac985e75f93b89a5e
singhpiyush998 <singhpiyush998@gmail.com>
2022-05-16 22:30:58 +0530

    f

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import {PrimaryButton} from '@fluentui/react/lib/Button'
+
 
 function App() {
   return (
     <div className='App'>
       <h1>Hello World</h1>
+      <PrimaryButton href='https://github.com/login'>Sign in with Github</PrimaryButton>
     </div>
   )
 }
